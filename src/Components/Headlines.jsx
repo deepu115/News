e16844cc2059f52dabfd17cfde79be6f72760048
Dileep <dileepvunnam@gmail.com>
2023-07-21 16:41:42 +0100

    headlines component

---
+++ src/Components/Headlines.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import mockNewsData from '../mockNewsData.json';
+
+const Headlines = () => {
+    return (
+        <div>
+
+            {mockNewsData.mockApiResponse.response.results.map((headline) => (
+                <div key={headline.id}>
+
+                    <img src={headline.fields.thumbnail} alt={headline.webTitle} />
+                    <h2>{headline.fields.headline}</h2>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default Headlines;
