24dbf8c0fa3e19854cd23fd513eda9dad542fd6b
Dileep <dileepvunnam@gmail.com>
2023-07-30 02:48:11 +0100

    json server

--- src/Utilities/data.js
+++ src/Utilities/data.js
@@ -1,16 +1,14 @@
 
 import axios from 'axios';
 
-const apiKey = '78734313-1940-4921-866a-2266df8f928a';
-const apiUrl = `https://content.guardianapis.com/search?order-by=newest&show-fields=byline%2Cthumbnail%2Cheadline%2CbodyText&api-key=${apiKey}`;
-
-export const fetchHeadlines = async (maxHeadlinesToShow) => {
+export const fetchHeadlines = async () => {
     try {
-        const response = await axios.get(apiUrl);
-        const headlines = response.data.response.results.slice(0, maxHeadlinesToShow);
-        return headlines;
+        const response = await axios.get('http://localhost:5000/mockApiResponse');
+
+        return response.data.response.results;
     } catch (error) {
-        console.error('Error Loading headlines:', error);
-        return [];
+        throw new Error('Error loading data.');
     }
+
 };
+
